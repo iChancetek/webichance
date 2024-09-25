a923922d06665f3df16f68048e8067153aaadf42
iChancetek <devops@ichancetek.com>
2024-09-25 12:28:54 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// script.js
+
+// Scroll to Top Button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Simple Form Validation
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["message"].value;
+    if (name == "" || email == "" || message == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+}
